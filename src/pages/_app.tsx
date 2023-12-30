97a4ba0015ee98e7de3b5cf1c78dde4cb051da78
philip82148 <92205270+philip82148@users.noreply.github.com>
2023-12-31 05:47:42 +0900

    add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
 import type { AppType } from 'next/app'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
+import Script from 'next/script'
 
 const App: AppType = ({ Component, pageProps }) => {
   const { basePath } = useRouter()
@@ -14,14 +15,30 @@ const App: AppType = ({ Component, pageProps }) => {
         <meta httpEquiv="Cache-Control" content="no-cache" />
         <meta httpEquiv="Pragma" content="no-cache" />
         <meta httpEquiv="Expires" content="0" />
-        <link
-          rel="stylesheet"
-          href="https://ss159178.stars.ne.jp/css/normalize.css?1349127987942"
-        />
+        {process.env.NODE_ENV !== 'development' && (
+          <link
+            rel="stylesheet"
+            href="https://ss159178.stars.ne.jp/css/normalize.css?1349127987942"
+          />
+        )}
 
         <title>舎生名簿生成器</title>
         <link rel="icon" href={basePath + '/favicon.png'} type="image/png" />
       </Head>
+      {process.env.NODE_ENV !== 'development' && (
+        <>
+          <Script async src="https://www.googletagmanager.com/gtag/js?id=G-E2VFB0PSST"></Script>
+          <Script id="google-analytics">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-E2VFB0PSST');
+            `}
+          </Script>
+        </>
+      )}
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Component {...pageProps} />
